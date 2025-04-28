4de92c75e2d9a4eb87234164880854ec53b025a2
Janani <jananiraja1405@gmail.com>
2025-04-28 15:42:39 +1200

    Revised

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -8,25 +8,25 @@ describe('Sign In page', () => {
   it('should login with Valid credentials', () => {
     cy.get('input[id="username"]').type('tomsmith');
     cy.get('input[id="password"]').type('SuperSecretPassword!');
-    cy.get('button[type="submit"]').should('contain.text', ' Login').click();
-    cy.get('a[href = "/logout"]').should('exist');
+    cy.get('button[type="submit"]').should('contain.text', 'Login').click();
+    cy.get('a[href="/logout"]').should('exist');
   });
 
   it('Should throw error when logged in with Invalid credentials', () => {
     cy.get('input[id="username"]').type('tomsmith');
     cy.get('input[id="password"]').type('SuperSecretPassword');
     cy.get('button[type="submit"]')
-      .should('contain.text', ' Login').click();
-    cy.get('div[class="flash error"]')
+      .should('contain.text', 'Login').click();
+    cy.get('div.flash.error')
       .should('contain.text', 'Your password is invalid!');
   });
 
   it('should be able to log out successfully', () => {
     cy.get('input[id="username"]').type('tomsmith');
     cy.get('input[id="password"]').type('SuperSecretPassword!');
-    cy.get('button[type="submit"]').should('contain.text', ' Login').click();
-    cy.get('a[href = "/logout"]').click();
-    cy.get('div[class="flash success"]')
+    cy.get('button[type="submit"]').should('contain.text', 'Login').click();
+    cy.get('a[href="/logout"]').click();
+    cy.get('div.flash.success')
       .should('contain.text', 'You logged out of the secure area!');
   });
 });
